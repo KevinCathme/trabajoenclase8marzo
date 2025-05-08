891537420dd9d5a59c1b50c4416c7bfb1c322798
KevinCathme <kevin.cathme@udla.edu.ec>
2025-05-08 16:54:07 -0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int lluvia[]= new int[12];
+        int lluvia[]= {23,24,20,21,26,22,23,23,22,25,22,20};
         String meses[]= {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+        int acumulada=0;
+
         for(int i=0;i<lluvia.length;i++){
-            System.out.println("Ingrese el dato del mes de "+meses[i]);
-            lluvia[i]=scanner.nextInt();
+                    System.out.print("El mes de "+meses[i]);
+                    System.out.print(" llovió "+lluvia[i]);
+                    System.out.println(" L/m2");
         }
+        for(int i=0;i<lluvia.length;i++){
+            acumulada=acumulada+lluvia[i];
+        }
+        System.out.print("La lluvia total acumulada es de "+acumulada+" L/m2");
     }
 }
