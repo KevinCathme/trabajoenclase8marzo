9865094fb707a2419a16c5b3d5df9944c9282f6d
KevinCathme <kevin.cathme@udla.edu.ec>
2025-05-08 17:32:44 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int lluvia[]= {23,24,20,21,26,22,23,23,22,25,22,20};
+        int lluvia[][]= new int[12][3];
+        lluvia[0][0] = 21; lluvia[0][1] = 25; lluvia[0][2] = 22;
+        lluvia[1][0] = 24; lluvia[1][1] = 23; lluvia[1][2] = 26;
+        lluvia[2][0] = 27; lluvia[2][1] = 20; lluvia[2][2] = 22;
+        lluvia[3][0] = 26; lluvia[3][1] = 24; lluvia[3][2] = 23;
+        lluvia[4][0] = 21; lluvia[4][1] = 22; lluvia[4][2] = 25;
+        lluvia[5][0] = 20; lluvia[5][1] = 26; lluvia[5][2] = 27;
+        lluvia[6][0] = 23; lluvia[6][1] = 25; lluvia[6][2] = 22;
+        lluvia[7][0] = 24; lluvia[7][1] = 20; lluvia[7][2] = 21;
+        lluvia[8][0] = 27; lluvia[8][1] = 23; lluvia[8][2] = 26;
+        lluvia[9][0] = 22; lluvia[9][1] = 24; lluvia[9][2] = 20;
+        lluvia[10][0] = 25; lluvia[10][1] = 26; lluvia[10][2] = 21;
+        lluvia[11][0] = 22; lluvia[11][1] = 23; lluvia[11][2] = 27;
         String meses[]= {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-        int acumulada=0;
-
-        for(int i=0;i<lluvia.length;i++){
-                    System.out.print("El mes de "+meses[i]);
-                    System.out.print(" llovió "+lluvia[i]);
-                    System.out.println(" L/m2");
-                    acumulada=acumulada+lluvia[i];
-        }
-        System.out.println("La lluvia total acumulada es de "+acumulada+" L/m2");
-        System.out.println("El promedio de lluvia mensual es de "+(acumulada/12)+" L/m2");
-        for(String nombreMes: meses){
-            System.out.println(nombreMes);
+        String ciudad[]= {"Quito","Guayaquill","Cuenca"};
+        int acumulada[]={0,0,0};
+        for(int i=0;i<3;i++){
+            for(int j=0;j<12;j++){
+                System.out.println("En la ciudad de "+ciudad[i]+" el mes de "+meses[j]+" llovió "+lluvia[j][i]+" L/m2");
+                acumulada[i]=acumulada[i]+lluvia[j][i];
+            }
+            System.out.println("La lluvia total de la ciudad "+ciudad[i]+" es de "+(acumulada[i])+"L/m2");
+            System.out.println("El promedio de lluvia de la ciudad "+ciudad[i]+" es de "+(acumulada[i]/12)+"L/m2");
         }
     }
 }
